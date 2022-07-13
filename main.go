bc6d2c012208e97a6eed27f66178bec255e1c7c9
Marcus Doescher <marcus.doescher@hmmh.de>
2022-07-13 14:49:13 +0200

    feat: add paddles

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"image/color"
 	"log"
-
-	ebiten "github.com/hajimehoshi/ebiten/v2"
 )
 
 const (
-	screenWidth  = 320
-	screenHeight = 240
+	screenWidth          = 1920
+	screenHeight         = 1080
+	margin       float64 = 150
+	paddleWidth          = screenWidth / 50
+	paddleHeight         = screenHeight / 4
 )
+
+type Paddle struct {
+	x      float64
+	y      float64
+	width  float64
+	height float64
+}
+
+func (p *Paddle) Draw(screen *ebiten.Image) {
+	ebitenutil.DrawRect(screen, p.x, p.y, p.width, p.height, color.White)
+}
+
+func NewPaddle(x float64) *Paddle {
+	return &Paddle{x, screenHeight/2 - paddleHeight/2, paddleWidth, paddleHeight}
+}
 
 type Game struct {
 }
@@ -19,11 +38,17 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+	for _, p := range []*Paddle{p1, p2} {
+		p.Draw(screen)
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return screenWidth, screenHeight
 }
+
+var p1 = NewPaddle(margin)
+var p2 = NewPaddle(screenWidth - margin - paddleWidth)
 
 func main() {
 	ebiten.SetWindowSize(1920, 1080)
